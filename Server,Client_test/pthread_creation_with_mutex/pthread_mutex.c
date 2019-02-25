027d408fe55338612faf2d5e60c42d6551c5e704
peterhyun1234 <peterhyun1234@gmail.com>
2019-02-25 16:55:26 +0900

    Add pthread creation with mutex

---
+++ Server,Client_test/pthread_creation_with_mutex/pthread_mutex.c
@@ -0,0 +1,65 @@
+//pthread_mutex.c
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+int ncount;
+pthread_mutex_t  mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void *do_sum1(void *data)
+{
+	int	i;
+	int	n = *((int *)data);
+
+	for(i = 0; i < n; i++)
+	{
+		printf("sum1 : %d\n", ncount);
+
+		pthread_mutex_lock(&mutex);
+		ncount ++;
+		pthread_mutex_unlock(&mutex);
+		sleep(1);
+	}
+}
+
+void *do_sum2(void *data)
+{
+	int	i;
+	int	n = *((int *)data);
+
+	for(i = 0; i < n; i++)
+	{
+		printf("sum2 : %d\n", ncount);
+
+		pthread_mutex_lock(&mutex);
+		ncount ++;
+		pthread_mutex_unlock(&mutex);
+		sleep(1);
+	}
+}
+
+int main( )
+{
+	int	thr_id;
+	pthread_t p_thread[2];
+	int	status;
+
+	int	a ;
+
+	ncount = 1;
+
+	a = 9;
+	thr_id = pthread_create(&p_thread[0], NULL, do_sum1, (void *) &a);
+	sleep(1);
+
+	a = 10;
+	thr_id = pthread_create(&p_thread[1], NULL, do_sum2, (void *) &a);
+
+	pthread_join(p_thread[0], (void **) &status);
+	pthread_join(p_thread[1], (void **) &status);
+
+	status = pthread_mutex_destroy(&mutex);
+	printf("programing is end");
+	return 0;
+}
+
